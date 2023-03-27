686128a143a69593abbfc46eb8aae5f18109973b
G_ARUN_KUMAR <103848853+aruniot099@users.noreply.github.com>
2023-03-27 16:46:30 +0530

    Add files via upload

--- code/src/main.ino
+++ code/src/main.ino
@@ -1,153 +1,131 @@
-#include <WiFi.h> 
-#include<Arduino.h>
- void sevenseg(int a,int b,int c,int d,int e,int f,int g)
+#if defined(ESP8266)
+  #include<ESP8266WiFi.h>
+//  #pragma message "ESP8266 stuff happening!"
+#elif defined(ESP32)
+  #include<WiFi.h>
+//  #pragma message "ESP32 stuff happening!"
+#else
+//  #error "This ain't a ESP8266 or ESP32"
+#endif
+
+//sudo chmod a+rw /dev/ttyUSB0 run this if got error
+WiFiClient client;
+WiFiServer server(80);
+
+// Set your Static IP address
+IPAddress local_IP(192, 168, 4, 4);
+// Set your Gateway IP address
+IPAddress gateway(192, 168, 1, 1);
+
+IPAddress subnet(255, 255, 0, 0);
+
+#define led 12
+
+void setup() 
 {
-  digitalWrite(32, a); 
-  digitalWrite(33, b); 
-  digitalWrite(25, c); 
-  digitalWrite(26, d); 
-  digitalWrite(27, e); 
-  digitalWrite(14, f);     
-  digitalWrite(12, g); 
+  // put your setup code here, to run once:
+Serial.begin(9600);
+  WiFi.softAP("ARUN_KUMAR", "arunkumar");
+  Serial.println();
+  Serial.println("NodeMCU Started!");
+  Serial.println(WiFi.softAPIP());
+  if (!WiFi.config(local_IP, gateway, subnet)) {
+  Serial.println("STA Failed to configure");
 }
-const char* ssid = "ARUN_KUMAR";         /*Enter Your SSID*/ 
-const char* password = "arunkumar"; /*Enter Your Password*/ 
- 
-WiFiServer server(80); /* Instance of WiFiServer with port number 80 */ 
-String request; 
- 
-WiFiClient client; 
- 
-void setup()  
-{ 
-  Serial.begin(115200);  
-  Serial.print("Connecting to: "); 
-  Serial.println(ssid); 
-  WiFi.mode(WIFI_STA); 
-  WiFi.begin(ssid, password); 
-  while(WiFi.status() != WL_CONNECTED) 
-  { 
-    Serial.print("."); 
-    delay(100); 
-  } 
-  Serial.print("\n"); 
-  Serial.print("Connected to Wi-Fi "); 
-  Serial.println(WiFi.SSID()); 
-  delay(1000); 
-  server.begin(); /* Start the HTTP web Server */ 
-  Serial.print("Connect to IP Address: "); 
-  Serial.print("http://"); 
-  Serial.println(WiFi.localIP()); 
-   pinMode(32, OUTPUT);  
-   pinMode(33, OUTPUT);
-   pinMode(25, OUTPUT);
-   pinMode(26, OUTPUT);
-   pinMode(27, OUTPUT);
-   pinMode(14, OUTPUT);
-   pinMode(12, OUTPUT);
-   pinMode(16, OUTPUT);
-   pinMode(17, OUTPUT);
-   pinMode(18, OUTPUT);
-   pinMode(19, OUTPUT);
+  Serial.println(WiFi.localIP());
+  server.begin();
+  pinMode(led, OUTPUT);
+}
 
-} 
- 
 void loop() 
-{ 
-  client = server.available(); 
-  if(!client) 
-  { 
-    return; 
-  } 
- 
-  while (client.connected()) 
-  { 
-    if (client.available()) 
-    {  
-      String req = client.readStringUntil('\r'); 
-      client.flush(); 
-      if(req.indexOf("/forward") != -1) 
-      {
-      Serial.println("Forward"); 
-      sevenseg(0,0,1,0,0,1,0); //2
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,HIGH); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/backward") != -1) 
-      {Serial.println("Backward"); 
-      sevenseg(0,0,0,0,1,1,0);  //3
-      digitalWrite(16,LOW); 
-      digitalWrite(17,HIGH); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,HIGH); 
-      } 
-      else if(req.indexOf("/left") != -1) 
-      {Serial.println("Left"); 
-      sevenseg(1,0,0,1,1,0,0);  //4
-      digitalWrite(16,LOW); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,HIGH); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/right") != -1) 
-      {Serial.println("Right"); 
-      sevenseg(0,1,0,0,1,0,0);  //5
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/stop") != -1) 
-      {Serial.println("Stop"); 
-      sevenseg(1,0,0,1,1,1,1); //1
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/start") != -1) 
-      {Serial.println("Start"); 
-      sevenseg(0,0,0,0,0,0,1); //0
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/cross") != -1) 
-      {Serial.println("Cross"); 
-      sevenseg(0,1,0,0,0,0,0);  //6
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/circle") != -1) 
-      {Serial.println("Circle"); 
-      sevenseg(0,0,0,1,1,1,1);   //7
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/square") != -1) 
-      {Serial.println("Square"); 
-      sevenseg(0,0,0,0,0,0,0); // 8
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-      else if(req.indexOf("/triangle") != -1) 
-      {Serial.println("Triangle"); 
-      sevenseg(0,0,0,0,1,0,0);   //9
-      digitalWrite(16,HIGH); 
-      digitalWrite(17,LOW); 
-      digitalWrite(18,LOW); 
-      digitalWrite(19,LOW); 
-      } 
-  } 
-  } 
+{
+  // put your main code here, to run repeatedly:
+  client = server.available();  //Gets a client that is connected to the server and has data available for reading.    
+  if (client == 1)
+  {  
+      String req = client.readStringUntil('\r');
+      client.flush();
+      if(req.indexOf("/forward") != -1)
+      {Serial.println("Forward");
+      ////digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,HIGH);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/backward") != -1)
+      {Serial.println("Backward");
+      //digitalWrite(16,LOW);
+      //digitalWrite(17,HIGH);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,HIGH);
+      }
+      else if(req.indexOf("/left") != -1)
+      {Serial.println("Left");
+      //digitalWrite(16,LOW);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,HIGH);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/right") != -1)
+      {Serial.println("Right");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/stop") != -1)
+      {Serial.println("Stop");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/start") != -1)
+      {Serial.println("Start");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/cross") != -1)
+      {Serial.println("Cross");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/circle") != -1)
+      {Serial.println("Circle");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/square") != -1)
+      {Serial.println("Square");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+      else if(req.indexOf("/triangle") != -1)
+      {Serial.println("Triangle");
+      //digitalWrite(16,HIGH);
+      //digitalWrite(17,LOW);
+      //digitalWrite(18,LOW);
+      //digitalWrite(19,LOW);
+      }
+  }
 }
 
+//    String request =  client.readStringUntil('\n');
+//    Serial.println(request);
+//    request.trim();
+//    if(request == "GET /ledon HTTP/1.1")
+//    {
+//      digitalWrite(led, HIGH);
+//    }
+//    if(request == "GET /ledoff HTTP/1.1")
+//    {
+//      digitalWrite(led, LOW);
+//    }
